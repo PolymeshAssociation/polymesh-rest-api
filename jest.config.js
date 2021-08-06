cc63327672ecfbec41b96b1fbcf98a897cac180f
Eric Richardson <ericrichardson@polymath.network>
2021-08-05 21:50:31 -0400

    Configure jest transformIgnorePatterns

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest/presets/js-with-babel',
+  transformIgnorePatterns: ['/node_modules/(?![@polymathnetwork/src]).+\\.js$'],
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: ['**/*.spec.(ts|tsx)'],
+  testPathIgnorePatterns: ['dist'],
+  moduleNameMapper: {
+    '~/(.*)': '<rootDir>/src/$1',
+    'polymesh-types/(.*)': '<rootDir>/src/polkadot/$1',
+  },
+};
