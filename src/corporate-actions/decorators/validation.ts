aa571343649c0e5180be575621ab248605e091b7
Prashant Bajpai <prashant.bajpai19@gmail.com>
2021-12-22 20:25:52 +0530

    refactor: 💡 Split `ca-checkpoint.ts` into separate files

--- src/corporate-actions/decorators/validation.ts
+++ src/corporate-actions/decorators/validation.ts
@@ -1,28 +1,9 @@
 /* istanbul ignore file */
 
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import { applyDecorators } from '@nestjs/common';
-import { plainToClass, Transform } from 'class-transformer';
 import { registerDecorator, validate as validateClass, ValidationArguments } from 'class-validator';
 
 import { CorporateActionCheckpointDto } from '~/corporate-actions/dto/corporate-action-checkpoint.dto';
-
-type CaCheckpoint = string | { type: string; id: number };
-
-/**
- * String | { type: string; id: string; } -> CorporateActionCheckpointDto | Date
- */
-export function ToCaCheckpoint() {
-  return applyDecorators(
-    Transform(({ value }: { value: CaCheckpoint }) => {
-      if (typeof value === 'string') {
-        return new Date(value);
-      } else {
-        return plainToClass(CorporateActionCheckpointDto, value);
-      }
-    })
-  );
-}
 
 export function IsCaCheckpoint() {
   // eslint-disable-next-line @typescript-eslint/ban-types
