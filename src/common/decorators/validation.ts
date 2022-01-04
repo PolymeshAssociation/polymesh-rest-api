1284d0bb92d453f7fdc3111d99fb271a3e1f4878
Prashant Bajpai <prashant.bajpai19@gmail.com>
2022-01-04 20:06:25 +0530

    refactor: 💡 Move `IsTxTag` and `IsTxTagOrModulename` to common

--- src/common/decorators/validation.ts
+++ src/common/decorators/validation.ts
@@ -16,6 +16,7 @@ import {
 
 import { MAX_TICKER_LENGTH } from '~/assets/assets.consts';
 import { DID_LENGTH } from '~/identities/identities.consts';
+import { getTxTags, getTxTagsWithModuleNames } from '~/identities/identities.util';
 
 export function IsDid(validationOptions?: ValidationOptions) {
   return applyDecorators(
@@ -55,6 +56,53 @@ export function IsBigNumber(validationOptions?: ValidationOptions) {
         },
         defaultMessage(args: ValidationArguments) {
           return `${args.property} must be a number`;
+        },
+      },
+    });
+  };
+}
+
+// TODO @prashantasdeveloper Reduce the below code from two decorators if possible - IsTxTag and IsTxTagOrModuleName
+export function IsTxTag(validationOptions?: ValidationOptions) {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: 'isTxTag',
+      target: object.constructor,
+      propertyName,
+      options: validationOptions,
+      validator: {
+        validate(value: unknown) {
+          return typeof value === 'string' && getTxTags().includes(value);
+        },
+        defaultMessage(args: ValidationArguments) {
+          if (validationOptions?.each) {
+            return `${args.property} must have all valid enum values`;
+          }
+          return `${args.property} must be a valid enum value`;
+        },
+      },
+    });
+  };
+}
+
+export function IsTxTagOrModuleName(validationOptions?: ValidationOptions) {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: 'isTxTagOrModuleName',
+      target: object.constructor,
+      propertyName,
+      options: validationOptions,
+      validator: {
+        validate(value: unknown) {
+          return typeof value === 'string' && getTxTagsWithModuleNames().includes(value);
+        },
+        defaultMessage(args: ValidationArguments) {
+          if (validationOptions?.each) {
+            return `${args.property} must have all valid enum values from "ModuleName" or "TxTags"`;
+          }
+          return `${args.property} must be a valid enum value from "ModuleName" or "TxTags"`;
         },
       },
     });
--- src/identities/decorators/validation.ts
+++ src/identities/decorators/validation.ts
@@ -1,9 +1,7 @@
 /* istanbul ignore file */
 
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import { registerDecorator, ValidationArguments, ValidationOptions } from 'class-validator';
-
-import { getTxTags, getTxTagsWithModuleNames } from '~/identities/identities.util';
+import { registerDecorator, ValidationArguments } from 'class-validator';
 
 export function IsPermissionsLike() {
   // eslint-disable-next-line @typescript-eslint/ban-types
@@ -21,53 +19,6 @@ export function IsPermissionsLike() {
         },
         defaultMessage(args: ValidationArguments) {
           return `${args.property} can have either 'transactions' or 'transactionGroups'`;
-        },
-      },
-    });
-  };
-}
-
-// TODO @prashantasdeveloper Reduce the below code from two decorators if possible - IsTxTag and IsTxTagOrModuleName
-export function IsTxTag(validationOptions?: ValidationOptions) {
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  return function (object: Object, propertyName: string) {
-    registerDecorator({
-      name: 'isTxTag',
-      target: object.constructor,
-      propertyName,
-      options: validationOptions,
-      validator: {
-        validate(value: unknown) {
-          return typeof value === 'string' && getTxTags().includes(value);
-        },
-        defaultMessage(args: ValidationArguments) {
-          if (validationOptions?.each) {
-            return `${args.property} must have all valid enum values`;
-          }
-          return `${args.property} must be a valid enum value`;
-        },
-      },
-    });
-  };
-}
-
-export function IsTxTagOrModuleName(validationOptions?: ValidationOptions) {
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  return function (object: Object, propertyName: string) {
-    registerDecorator({
-      name: 'isTxTagOrModuleName',
-      target: object.constructor,
-      propertyName,
-      options: validationOptions,
-      validator: {
-        validate(value: unknown) {
-          return typeof value === 'string' && getTxTagsWithModuleNames().includes(value);
-        },
-        defaultMessage(args: ValidationArguments) {
-          if (validationOptions?.each) {
-            return `${args.property} must have all valid enum values from "ModuleName" or "TxTags"`;
-          }
-          return `${args.property} must be a valid enum value from "ModuleName" or "TxTags"`;
         },
       },
     });
