647d392909187efe89ae53c6226b628ea02de89d
Eric <ericrichardson@polymath.network>
2022-04-04 16:31:11 -0400

    refactor: 💡 Move setSigningManager call to base class

--- src/signing/signing.service.ts
+++ src/signing/signing.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { HashicorpVaultSigningManager } from '@polymathnetwork/hashicorp-vault-signing-manager';
 import { LocalSigningManager } from '@polymathnetwork/local-signing-manager';
+import { SigningManager } from '@polymathnetwork/signing-manager-types';
 import _ from 'lodash';
 
 import { PolymeshLogger } from '~/logger/polymesh-logger.service';
@@ -8,9 +9,14 @@ import { PolymeshService } from '~/polymesh/polymesh.service';
 
 @Injectable()
 export abstract class SigningService {
+  protected readonly signingManager: SigningManager;
+  protected readonly polymeshService: PolymeshService;
+
   public abstract getAddressByHandle(handle: string): Promise<string>;
 
-  public abstract initialize(accounts?: Record<string, string>): Promise<void>;
+  public async initialize(): Promise<void> {
+    return this.polymeshService.polymeshApi.setSigningManager(this.signingManager);
+  }
 
   protected throwNoSigner(handle: string): never {
     throw new NotFoundException(`There is no signer associated to "${handle}"`);
@@ -21,8 +27,8 @@ export class LocalSigningService extends SigningService {
   private addressBook: Record<string, string> = {};
 
   constructor(
-    private readonly signingManager: LocalSigningManager,
-    private readonly polymeshService: PolymeshService,
+    protected readonly signingManager: LocalSigningManager,
+    protected readonly polymeshService: PolymeshService,
     private readonly logger: PolymeshLogger
   ) {
     super();
@@ -44,10 +50,9 @@ export class LocalSigningService extends SigningService {
   }
 
   public async initialize(accounts: Record<string, string> = {}): Promise<void> {
-    const manager = this.signingManager;
-    await this.polymeshService.polymeshApi.setSigningManager(manager);
+    await super.initialize();
     _.forEach(accounts, (mnemonic, handle) => {
-      const address = manager.addAccount({ mnemonic });
+      const address = this.signingManager.addAccount({ mnemonic });
       this.setAddressByHandle(handle, address);
       this.logKey(handle, address);
     });
@@ -60,16 +65,16 @@ export class LocalSigningService extends SigningService {
 
 export class VaultSigningService extends SigningService {
   constructor(
-    private readonly signingManager: HashicorpVaultSigningManager,
-    private readonly polymeshService: PolymeshService,
+    protected readonly signingManager: HashicorpVaultSigningManager,
+    protected readonly polymeshService: PolymeshService,
     private readonly logger: PolymeshLogger
   ) {
     super();
     this.logger.setContext(VaultSigningService.name);
   }
 
-  public initialize(): Promise<void> {
-    this.polymeshService.polymeshApi.setSigningManager(this.signingManager);
+  public async initialize(): Promise<void> {
+    await super.initialize();
     return this.logKeys();
   }
 
