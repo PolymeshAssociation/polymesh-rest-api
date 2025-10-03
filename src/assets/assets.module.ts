83b1712bc4c07005bd777186b5946a3fa20f8653
Toms <toms@veidemanis.lv>
2025-10-03 12:41:25 +0300

    fix: 🐛 release stage

--- src/assets/assets.module.ts
+++ src/assets/assets.module.ts
@@ -4,12 +4,18 @@ import { forwardRef, Module } from '@nestjs/common';
 
 import { AssetsController } from '~/assets/assets.controller';
 import { AssetsService } from '~/assets/assets.service';
+import { IdentitiesModule } from '~/identities/identities.module';
 import { MetadataModule } from '~/metadata/metadata.module';
 import { PolymeshModule } from '~/polymesh/polymesh.module';
 import { TransactionsModule } from '~/transactions/transactions.module';
 
 @Module({
-  imports: [PolymeshModule, TransactionsModule, forwardRef(() => MetadataModule)],
+  imports: [
+    PolymeshModule,
+    TransactionsModule,
+    forwardRef(() => MetadataModule),
+    forwardRef(() => IdentitiesModule),
+  ],
   controllers: [AssetsController],
   providers: [AssetsService],
   exports: [AssetsService],
